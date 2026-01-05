5f3a5ead740ac06014dbb14d44241db65d914bbf
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2026-01-05 15:47:52 +0700

    add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, TodosModule, AuthModule],
+  imports: [UsersModule, TodosModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
